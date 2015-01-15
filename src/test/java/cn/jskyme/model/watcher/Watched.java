964d94f84be906d98292c20c19a92b04b2064c76
jskyme <jskyme@gmail.com>
2015-01-16 02:45:09 +0800

    init

---
+++ src/test/java/cn/jskyme/model/watcher/Watched.java
@@ -0,0 +1,10 @@
+package cn.jskyme.model.watcher;
+
+public interface Watched {
+	public void addWatcher(Watcher watcher);
+
+    public void removeWatcher(Watcher watcher);
+
+    public void notifyWatchers(String str);
+
+}
